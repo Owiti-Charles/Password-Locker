97651cf5437517770c2a28ec7d01905194644cda
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-30 18:56:05 +0300

    Add second test to test save_user method

--- passlock.py
+++ passlock.py
@@ -3,11 +3,18 @@ class User:
     Create User class that generates new instances of a user.
 
     """
-    user = []
-    
+    user_list = []
+
     def __init__(self, username, password):
         """
         method that defines the properties of a user.
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
+
--- passlock_test.py
+++ passlock_test.py
@@ -1,25 +1,30 @@
 import unittest
 from passlock import User
 
-class UserTest(unittest.TestCase):
-    """
-    A Test class that defines test cases for the User class.
-
-    """
+class TestClass(unittest.TestCase):
+    # """
+    # A Test class that defines test cases for the User class.
+    # """
     def setUp(self):
         """
         Method that runs before each individual test methods run.
-
         """
         self.new_user = User('OwitiCharles','XyZ3thf1')
-    
+
     def test_init(self):
         """
         test case to chek if the object has been initialized correctly
-
         """
         self.assertEqual(self.new_user.username,'OwitiCharles')
         self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
 if __name__ == "__main__":
     unittest.main()
