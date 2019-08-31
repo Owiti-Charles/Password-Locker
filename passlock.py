d058c397e494d89b29fb05012b42e9571030ead6
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-31 17:00:44 +0300

    Add mentod to generate new random passwords

--- passlock.py
+++ passlock.py
@@ -87,5 +87,10 @@ class Credentials():
         """
         return cls.credentials_list
 
+    def generatePassword(self,stringLength=8):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
+
 
 
