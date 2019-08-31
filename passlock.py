9566fb75a9e95064e7d644c0944460efab30561b
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-31 17:03:23 +0300

    Create a new interface.py file where the code that allows the user to interact with the application will be put

--- passlock.py
+++ passlock.py
@@ -91,6 +91,3 @@ class Credentials():
         """Generate a random password string of letters and digits and special characters"""
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
         return ''.join(random.choice(password) for i in range(stringLength))
-
-
-
